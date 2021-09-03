4d9cb58b90b8cd885a3f3b54e48ef22c3488e836
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-03 21:11:00 +0300

    Add files via upload

--- backend.php
+++ backend.php
@@ -1,7 +1,7 @@
 <?php
     include_once('index.html');
 
-    $str = $_GET['num1'];
+    $str = $_POST['num1'];
     $fd = file_get_contents('data.txt');
     echo $fd;
     file_put_contents('data.txt', $str, FILE_APPEND)
