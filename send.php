0383ef00659d58dfc120b33fa35fcc8380c4164e
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-04 01:43:27 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
     $str = $_POST['str'];
     echo $str;
-    $ff = file_put_contents('data.php', '<p>' . $str . '</p>', FILE_APPEND);
+    $ff = file_put_contents('data.php', '<p>' . $str . '</p>', FILE_APPEND | FILE_USE_INCLUDE_PATH);
     echo $ff
 ?>
