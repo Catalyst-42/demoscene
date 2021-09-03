93609e957296a41b425e96c924ac9b8ebc0d7f68
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-04 01:59:35 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php    
+<?php
+    $str = $_POST['str'];
+    file_put_contents('data.php', '<p>' . $str . "</p>", FILE_APPEND);
+    
     include_once('index.html');
     include_once('data.php');
 ?>
