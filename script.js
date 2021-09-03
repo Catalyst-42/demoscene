6b78b7f63e41908f4012f88956d0b7aea731fe76
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-03 23:18:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-document.querySelector('.send').addEventListener('click', function () {
-    console.log(document.querySelector('input').value);
-    
-    xhttp.open("POST", "http://demoscene.herokuapp.com/send.php")
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send('str='+document.querySelector('input').value);
-    
-    document.querySelector('input').value = '';
-})
-
 let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         addAnswer(this.responseText);
     }
 }
+
+document.querySelector('.send').addEventListener('click', function () {
+    console.log(document.querySelector('input').value);
+    
+    xhttp.open("POST", "http://demoscene.herokuapp.com/file_save.php")
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send('str='+document.querySelector('input').value);
+    
+    document.querySelector('input').value = '';
+})
 
 function addAnswer (data) {
     let p = document.createElement('p')
