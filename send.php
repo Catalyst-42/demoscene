09948673dc82177c9008903702c93c6a9907557e
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-04 01:53:46 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,5 @@
 <?php
     $str = $_POST['str'];
     echo $str;
-    $ff = file_put_contents(__DIR__.'data.txt', '<p>' . $str . '</p>', FILE_APPEND | FILE_USE_INCLUDE_PATH);
-    echo $ff
+    file_put_contents(__DIR__.'data.txt', '<p>' . $str . '</p>', FILE_APPEND | FILE_USE_INCLUDE_PATH);
 ?>
