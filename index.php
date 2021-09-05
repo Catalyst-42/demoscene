6c1f3094ea9ec2449fe1e01c5cf47a808caea181
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-05 15:03:09 +0300

    wait....

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     include_once('data.php');
 
     $link = mysqli_connect("us-cdbr-east-04.cleardb.com", "bae47087acc5a8", "69125eeb");
-    $sql = 'SELECT comment FROM near';
+    $sql = 'SELECT comment FROM near;';
     $result = mysqli_query($link, $sql);
     
     $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);    
