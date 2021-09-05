356294463fd5ee76c7cd59e5bfcdf5c07dd20e02
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-05 15:24:57 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -5,6 +5,15 @@
     // include_once('data.php');
 
     $link = mysqli_connect("us-cdbr-east-04.cleardb.com", "bae47087acc5a8", "69125eeb");
+
+    if ($link == false) {
+        echo "Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error();
+    }
+    else {
+        print("Соединение установлено успешно");
+        mysqli_set_charset($link, "utf8");
+    }
+
     $sql = "INSERT near(comment) VALUES('" . $str . "');";
     $result = mysqli_query($link, $sql);
 
