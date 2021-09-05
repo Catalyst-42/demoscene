b5e14eb7be39533c890b27574803be1cd5f8f4b5
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-05 13:51:41 +0300

    idk

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<?php
-    $str = $_POST['str'];
-    file_put_contents('data.php', '<p>' . $str . "</p>", FILE_APPEND);
-    
+<?php    
     include_once('index.html');
     include_once('data.php');
+
+
+    $link = mysqli_connect("us-cdbr-east-04.cleardb.com", "bae47087acc5a8", "69125eeb");
+    $sql = 'SELECT id, name FROM comment';
+    $result = mysqli_query($link, $sql);
+
+    while ($row = mysqli_fetch_array($result)) {
+        print("Город: " . $row['name'] . "; Идентификатор: . " . $row['id'] . "<br>");
+    }
 ?>
--- send.php
+++ send.php
@@ -1,6 +1,24 @@
 <?php
     $str = $_POST['str'];
     echo $str;
-    file_put_contents('data.php', '<p>' . $str . '</p>', FILE_APPEND);
-    include_once('data.php');
+    // file_put_contents('data.php', '<p>' . $str . '</p>', FILE_APPEND);
+    // include_once('data.php');
+
+    $link = mysqli_connect("us-cdbr-east-04.cleardb.com", "bae47087acc5a8", "69125eeb");
+
+    if ($link == false) {
+        print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+    }
+    else {
+        print("Соединение установлено успешно");
+        mysqli_set_charset($con, "utf8");
+    }
+
+    $sql = 'INSERT INTO near SET comment = "'  . $str . '"';
+    $result = mysqli_query($link, $sql);
+
+    if ($result == false) {
+        print("Произошла ошибка при выполнении запроса");
+    }
+
 ?>
