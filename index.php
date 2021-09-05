51e2c74f248c805e1bfed52b0b26b8fcb5ca52f5
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-05 14:02:46 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     include_once('index.html');
     include_once('data.php');
 
-
     $link = mysqli_connect("us-cdbr-east-04.cleardb.com", "bae47087acc5a8", "69125eeb");
     $sql = 'SELECT id, name FROM comment';
     $result = mysqli_query($link, $sql);
