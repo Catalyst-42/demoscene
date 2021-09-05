d24073248feb46f325a774d0c6613ce21e22e195
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-05 16:08:51 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,14 +3,6 @@
 
     $link = mysqli_connect("us-cdbr-east-04.cleardb.com", "bae47087acc5a8", "69125eeb");
 
-    if ($link == false) {
-        echo "Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error();
-    }
-    else {
-        echo "Соединение установлено успешно";
-        // mysqli_set_charset($link, "utf8");
-    }
-
     $sql = 'SELECT comment FROM near';
     $result = mysqli_query($link, $sql);
 
--- send.php
+++ send.php
@@ -6,19 +6,12 @@
 
     $link = mysqli_connect("us-cdbr-east-04.cleardb.com", "bae47087acc5a8", "69125eeb");
 
-    if ($link == false) {
-        echo "Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error();
-    }
-    else {
-        print("Соединение установлено успешно");
-        // mysqli_set_charset($link, "utf8");
-    }
-
     $sql = "INSERT INTO near SET comment = '" . $str . "'";
     $result = mysqli_query($link, $sql);
 
     if ($result == false) {
         echo "Произошла ошибка при выполнении запроса";
+        echo mysqli_error($link);
     }
 
 ?>
