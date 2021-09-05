6c585c21fe10c8bef6d91d5b8ed8a4cc80e4ec4d
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-05 15:51:37 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,26 +1,42 @@
 <?php    
     include_once('index.html');
-    include_once('data.php');
 
+    // $link = mysqli_connect("us-cdbr-east-04.cleardb.com", "bae47087acc5a8", "69125eeb");
+
+    // if ($link == false) {
+    //     echo "Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error();
+    // }
+    // else {
+    //     echo "Соединение установлено успешно";
+    //     // mysqli_set_charset($link, "utf8");
+    // }
+
+    // $sql = 'SELECT comment FROM near';
+    // $result = mysqli_query($link, $sql);
+
+    // if ($result == false) {
+    //     echo '<br>результат ложный';
+    // }
+
+    // while ($row = mysqli_fetch_array($result)) {
+    //     echo "Город: " . $row['comment'] . "<br>";
+    // }
+    
+    $str = 'hello i just wanna to store string!!!';
     $link = mysqli_connect("us-cdbr-east-04.cleardb.com", "bae47087acc5a8", "69125eeb");
 
     if ($link == false) {
         echo "Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error();
     }
     else {
-        echo "Соединение установлено успешно";
+        print("Соединение установлено успешно");
         // mysqli_set_charset($link, "utf8");
     }
 
-    $sql = 'SELECT comment FROM near';
+    $sql = "INSERT near(comment) VALUES('" . $str . "')";
     $result = mysqli_query($link, $sql);
 
     if ($result == false) {
-        echo '<br>результат ложный';
-        echo $result;
-    }
-
-    while ($row = mysqli_fetch_array($result)) {
-        echo "Город: " . $row['comment'] . "<br>";
+        echo "Произошла ошибка при выполнении запроса";
     }
 ?>
