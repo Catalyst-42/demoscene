608366ef3d294961a6ffb085c9fa17707d5b6421
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-05 15:10:24 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,9 +5,8 @@
     $link = mysqli_connect("us-cdbr-east-04.cleardb.com", "bae47087acc5a8", "69125eeb");
     $sql = 'SELECT comment FROM near;';
     $result = mysqli_query($link, $sql);
-    
-    $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);    
-    foreach ($rows as $row) {
-        echo "Город: " . $row['comment'] . "; Идентификатор: . <br>";
+
+    while ($row = mysqli_fetch_array($result)) {
+        print("Город: " . $row['comment'] . "<br>");
     }
 ?>
