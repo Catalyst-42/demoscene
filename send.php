e1fc75b60519004b61c31f5886f9fd7b7679a53e
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-07 20:44:26 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,28 +1,16 @@
 <?php
     $str = $_POST['str'];
-    
+    echo $str;
+
     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $server = $url["host"];
     $username = $url["user"];
     $password = $url["pass"];
     $db = substr($url["path"], 1);
-    
+
     $link = new mysqli($server, $username, $password, $db);
     mysqli_set_charset($link, "utf8");
-    
-    $sql = "INSERT INTO near(comment, data) VALUES ('$str', NOW())";
-    $result = mysqli_query($link, $sql);
-    
-    // $result = mysqli_query($link,  "SELECT MAX(id) FROM near");
-    // $row = mysqli_fetch_row($result);
-    // echo $row[0];
-    
-    // echo $str;
 
-    $sql = 'SELECT * FROM near ORDER BY id DESC LIMIT 0, 1';
+    $sql = "INSERT INTO near SET comment = '$str'";
     $result = mysqli_query($link, $sql);
-
-    while ($row = mysqli_fetch_array($result)) {
-        echo "<p class='comment' id='" . $row['id'] . "'>" . $row['data'] . '<br>'. $row['comment'] . "</p>";
-    }
 ?>
