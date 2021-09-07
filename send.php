b1233a6141001f7ea096c2e6429cb335ef08fff2
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-08 00:05:03 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -11,14 +11,22 @@
     $link = new mysqli($server, $username, $password, $db);
     mysqli_set_charset($link, "utf8");
 
-    $sql = "INSERT INTO near(comment, data) VALUES ('$str', NOW())";
-    $result = mysqli_query($link, $sql);
+    // $sql = "INSERT INTO near(comment, data) VALUES ('$str', NOW())";
+    // $result = mysqli_query($link, $sql);
 
     // $sql = "SELECT id FROM near ORDER BY id DESC LIMIT 0, 1";
-    $sql = "SELECT comment, data, id FROM near WHERE id>=15 ORDER BY id";
+    $sql = "SELECT comment, data, id FROM near WHERE id>=10 ORDER BY id";
     $result = mysqli_query($link, $sql);
     $row = mysqli_fetch_array($result);
     $data[] = $row;
 
     echo json_encode($data);
 ?>
+
+[
+    {"0":"hi2",
+    "comment":"hi2",
+    "1":"2021-09-07 20:24:39",
+    "data":"2021-09-07 20:24:39",
+    "2":"15"
+    "id":"15"}]
