0f5ef180f3c49ce36cd2cb9284c78657a5702b5a
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-07 18:34:00 +0300

    last update

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-let xhttp = new XMLHttpRequest();
+let xhttp = new XMLHttpRequest()
 xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        addAnswer(this.responseText);
+        addAnswer(this.responseText)
     }
-}
-
-setInterval(update, 5000)
-function update () {
-    // xhttp.open("POST", "https://demoscene.herokuapp.com/update.php")
-    // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");    
-    // xhttp.send('id=' + last_id);
 }
 
 document.querySelector('.send').addEventListener('click', function () {    
     xhttp.open("POST", "https://demoscene.herokuapp.com/send.php")
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");    
-    xhttp.send('str=' + document.querySelector('.input').value);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+    xhttp.send('str=' + document.querySelector('.input').value)
     
-    document.querySelector('.input').value = '';
+    document.querySelector('.input').value = ''
 })
 
 function addAnswer (data) {
