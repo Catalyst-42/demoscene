0c7e381971761ee2fe55f02fd7e0e71f4d1b72e2
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-07 20:29:57 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -13,8 +13,16 @@
     $sql = "INSERT INTO near(comment, data) VALUES ('$str', NOW())";
     $result = mysqli_query($link, $sql);
     
-    $result = mysqli_query($link,  "SELECT MAX(id) FROM near");
-    $row = mysqli_fetch_row($result);
-    echo $row[0];
-    echo $str;
+    // $result = mysqli_query($link,  "SELECT MAX(id) FROM near");
+    // $row = mysqli_fetch_row($result);
+    // echo $row[0];
+    
+    // echo $str;
+
+    $sql = 'SELECT * FROM near ORDER BY id DESC LIMIT 0, 1';
+    $result = mysqli_query($link, $sql);
+
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<p class='comment' id='" . $row['id'] . "'>" . $row['data'] . '<br>'. $row['comment'] . "</p>";
+    }
 ?>
