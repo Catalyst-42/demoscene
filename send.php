4a28d64dff866a7af52a64d890ec0d79c8a765d2
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-07 18:20:27 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,6 +1,21 @@
 <?php
     $str = $_POST['str'];
     echo $str;
+    
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+    
+    $link = new mysqli($server, $username, $password, $db);
+    mysqli_set_charset($link, "utf8");
+    
+    $sql = "INSERT INTO near(comment, data) VALUES ('$str', NOW())";
+    $result = mysqli_query($link, $sql);
+    
+    echo "<meta http-equiv='refresh' content='0'>";
+    include_once('index.html');
 
     $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
     $server = $url["host"];
@@ -11,6 +26,10 @@
     $link = new mysqli($server, $username, $password, $db);
     mysqli_set_charset($link, "utf8");
 
-    $sql = "INSERT INTO near(comment, data) VALUES ('$str', NOW())";
-    $result = mysqli_query($link, $sql)
+    $sql = 'SELECT comment, data, id FROM near ORDER BY id';
+    $result = mysqli_query($link, $sql);
+
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<p class='comment' id='" . $row['id'] . "'>" . $row['data'] . '<br>'. $row['comment'] . "</p>";
+    }
 ?>
