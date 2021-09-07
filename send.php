dc41cd039d5869a81ec0c2fbfb38c9b154ecdc40
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-07 20:09:09 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -16,5 +16,6 @@
     $sql = "SELECT MAX(id) FROM near";
     $result = mysqli_query($link, $sql);
 
-    echo $str, mysqli_fetch_array($result)['id'];
+    echo $str;
+    echo mysqli_fetch_array($result);
 ?>
