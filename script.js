a0ebfa5641b10a35bb6df05c7dd0801d32be2fe9
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-08 10:16:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function addAnswer (data) {
     // let minutes = date_ob.getMinutes()
     // let seconds = date_ob.getSeconds()
 
-    data = JSON.parse(data)
+    data = JSON.parse('{' + data + '}')
 
     for (i in data.length()) {
         let p = document.createElement('p')
