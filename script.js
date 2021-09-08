8fc0c47db29d16b1d7ecc717303cc35afbec299c
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-08 10:06:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,24 +16,23 @@ document.querySelector('.send').addEventListener('click', function () {
 })
 
 function addAnswer (data) {
-    let date_ob = new Date()
-    let date = ("0" + date_ob.getDate()).slice(-2)
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2)
-    let year = date_ob.getFullYear()
-    let hours = date_ob.getHours()
-    let minutes = date_ob.getMinutes()
-    let seconds = date_ob.getSeconds()
-    let p = document.createElement('p')
-    p.classList.add('comment')
 
-    p.innerHTML =  year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds + '<br>' + data
-    document.body.querySelector('.comments').appendChild(p)
+    // let date_ob = new Date()
+    // let date = ("0" + date_ob.getDate()).slice(-2)
+    // let month = ("0" + (date_ob.getMonth() + 1)).slice(-2)
+    // let year = date_ob.getFullYear()
+    // let hours = date_ob.getHours()
+    // let minutes = date_ob.getMinutes()
+    // let seconds = date_ob.getSeconds()
+
+    for (i in data) {
+        let p = document.createElement('p')
+        p.classList.add('comment')
+        p.id = i[2]
+        
+        // p.innerHTML =  year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds + '<br>' + data
+        p.innerHTML =  i[1] + "<br>" + i[0]
+        document.body.querySelector('.comments').appendChild(p)
+    }
+
 }
-
-[
-    {"0":"hi2",
-    "comment":"hi2",
-    "1":"2021-09-07 20:24:39",
-    "data":"2021-09-07 20:24:39",
-    "2":"15",
-    "id":"15"}]
