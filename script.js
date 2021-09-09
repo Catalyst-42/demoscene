21fbe0df39a72a2a0fe68c14761e05dbdbc8b56e
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-09 20:21:24 +0300

    style update

--- script.js
+++ script.js
@@ -30,9 +30,7 @@ function addAnswer (data) {
         let p = document.createElement('p')
         p.classList.add('comment')
         p.id = data[i]['id']
-
-        // p.innerHTML =  year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds + '<br>' + data
-        p.innerHTML =  "<span class='bg'>" + data[i]['data']  + "</span" + "<br>" + data[i]['comment']
+        p.innerHTML =  "<span class='bg'>" + data[i]['data']  + "</span><br>" + data[i]['comment']
         document.body.querySelector('.comments').appendChild(p)
     }
 }
