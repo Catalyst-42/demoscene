e9987b991e8b5b87c0def98500b105803285fb94
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-11 20:20:54 +0300

    polish

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function addAnswer (data) {
         let p = document.createElement('p')
         p.classList.add('comment')
         p.id = data[i]['id']
-        p.innerHTML =  "<span class='bg'>" + data[i]['data']  + "</span><br>" + data[i]['comment']
+        p.innerHTML =  "<span class='bg'>" + data[i]['data']  + "</span><br>" + data[i]['comments']
         document.body.querySelector('.comments').appendChild(p)
     }
 }
