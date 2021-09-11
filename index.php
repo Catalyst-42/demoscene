3438e3142e910dce0e07bce06f85e3c18ac1809d
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2021-09-11 19:31:21 +0300

    polish

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap" rel="stylesheet">
       
-      <title>StageL</title>
+      <title>StageT</title>
     </head>
     
     <body>
