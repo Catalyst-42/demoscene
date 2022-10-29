5d261662a78baefad408b54d4970e21ddd2fca02
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2022-10-29 17:39:10 +0300

    scripts temporarily forbidden

--- send.php
+++ send.php
@@ -20,7 +20,8 @@
     }
     mysqli_set_charset($link, "utf8");
 
-    $str = strip_tags($str, '<p><a><style><div><marquee><span><s><u><b><i><br>');
+    // scripts temporarily forbidden
+    $str = strip_tags($str, '<p><a><style><div><marquee><span><s><u><b><i><br><h1><h2><h3><img><button>');
     if ($str != '') {
         $sql = "INSERT INTO near(comments, data) VALUES ('$str', NOW())";
         $result = mysqli_query($link, $sql);
