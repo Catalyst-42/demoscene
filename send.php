3920b503e2eb8322038306e01c5caf473e716dd4
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2022-10-30 20:07:59 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -21,7 +21,7 @@
     mysqli_set_charset($link, "utf8");
 
     // scripts and styles temporarily forbidden
-    // $str = strip_tags($str);
+    $str = strip_tags($str, "<b><s><u><i><a><pre><style><p><div><marquee><h1><h2><h3><span>");
     if ($str != '') {
         $sql = "INSERT INTO near(comments, data) VALUES ('$str', NOW())";
         $result = mysqli_query($link, $sql);
