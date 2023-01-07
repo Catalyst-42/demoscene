4e42a5c81e899633a24c13391adabede4514ffb6
Catalyst-42 <catalyst42c@gmail.com>
2023-01-08 00:38:33 +0300

    alpha2.4

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
-    if (isset(urlencode($_POST['str']))) { $str = urlencode($_POST['str']); } else { $str = ''; }
+    if (isset($_POST['str'])) { $str = urldencode($_POST['str']); } else { $str = ''; }
     $id = $_POST['id'];
 
     try {
