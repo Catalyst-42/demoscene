79ccf64433764d6f4b55fe8e36d8e7c1850c83ae
Catalyst-42 <catalyst42c@gmail.com>
2023-01-08 15:53:23 +0300

    alpha2.4

--- send.php
+++ send.php
@@ -9,38 +9,36 @@
     }
 
     mysqli_set_charset($link, "utf8");
-    $str = str_replace(array('<', '>'), array('&lt;', '&gt;'), $str);
     
-    // standard tags
-    $str = preg_replace('/\[(\/?([subi]))]/', '<${1}>', $str);
-    
-    // custom tags
-    $str = preg_replace('/\[((rainbow|magic|silver|jump|shake))]/', '<span class="${1}-animated">', $str);
-    $str = preg_replace('/\[(\/(rainbow|magic|silver|jump|shake|))]/', '</span>', $str);
-    
-    // colors
-    $str = preg_replace('/\[(#([0-9a-fA-F]{3}){1,2})\]/', '<span style="color: ${1};">', $str);
-
     if ($str != '') {
+        $str = str_replace(array('<', '>'), array('&lt;', '&gt;'), $str);
+        
+        // standard tags
+        $str = preg_replace('/\[(\/?([subi]))]/', '<${1}>', $str);
+        
+        // custom tags
+        $str = preg_replace('/\[((rainbow|magic|silver|jump|shake))]/', '<span class="${1}-animated">', $str);
+        $str = preg_replace('/\[(\/(rainbow|magic|silver|jump|shake|))]/', '</span>', $str);
+        
+        // colors
+        $str = preg_replace('/\[(#([0-9a-fA-F]{3}){1,2})\]/', '<span style="color: ${1};">', $str);
+
         $sql = $link->prepare('INSERT INTO near(comments, data) VALUES (?, NOW());');
         $sql->bind_param('s', $str);
         $sql->execute();
-        
-        $result = mysqli_query($link, $sql);
         $sql->close();
     }
     
     $sql = $link->prepare('SELECT comments, data, id FROM near WHERE id>? ORDER BY id;');
     $sql->bind_param('i', $id);
     $sql->execute();
-
-    $result = mysqli_query($link, $sql);
     $sql->close();
     
     $types = array();
     while($row = mysqli_fetch_assoc($result)) {
         array_push($types, array('comments' => $row['comments'], 'data' => $row['data'], 'id' => $row['id']));
     }
-    
+
+    $link->close();
     echo json_encode($types, JSON_UNESCAPED_UNICODE);
 ?>
