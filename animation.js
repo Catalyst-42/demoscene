166e3500b02407d26defb3faf612532a28dafc70
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2023-08-09 20:23:41 +0300

    lazy loading mode

--- animation.js
+++ animation.js
@@ -1,7 +1,19 @@
+$.fn.isInViewport = function() {
+  let elementTop = $(this).offset().top
+  let elementBottom = elementTop + $(this).outerHeight()
+
+  let viewportTop = $(window).scrollTop()
+  let viewportBottom = viewportTop + $(window).height()
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
 function applyAnimations() {
   // apply shake effect
   $(".shake-animated").not(".compiled").each(function() {
+    if (!$(this).isInViewport()) return
     $(this).addClass("compiled")
+    
     let text = $(this).text().split("")
     let modified_text = ""
 
@@ -17,20 +29,22 @@ function applyAnimations() {
 
   // apply jump effect
   $(".jump-animated").not(".compiled").each(function() {
+    if (!$(this).isInViewport()) return
     $(this).addClass("compiled")
-      let jump_index = 0
-      let text = $(this).text().split("")
-      let modified_text = ""
 
-      for (var i = 0; i < text.length; i++) {
-        jump_index += 0.05
-        if (text[i] === ' ' & text[i-1] != ' ') {
-          modified_text += '<pre style="margin: 0"> </pre>'
-        } else {
-          modified_text += `<span class="jump" style="animation-delay: ${jump_index}s;">${text[i]}</span>`
-        }
+    let jump_index = 0
+    let text = $(this).text().split("")
+    let modified_text = ""
+
+    for (var i = 0; i < text.length; i++) {
+      jump_index += 0.05
+      if (text[i] === ' ' & text[i-1] != ' ') {
+        modified_text += '<pre style="margin: 0"> </pre>'
+      } else {
+        modified_text += `<span class="jump" style="animation-delay: ${jump_index}s;">${text[i]}</span>`
       }
-      $(this).html(modified_text)
+    }
+    $(this).html(modified_text)
   })
   
   // animate magic star
@@ -46,7 +60,9 @@ function applyAnimations() {
   
   // apply magic effect
   $(".magic-animated").not(".compiled").each(function() {
+    if (!$(this).isInViewport()) return
     $(this).addClass("compiled")
+
     for (let i = 0; i < 3; i++) {
       $(this).prepend("<span class='magic-star'><svg viewBox='0 0 512 512'><path d='M512 255.1c0 11.34-7.406 20.86-18.44 23.64l-171.3 42.78l-42.78 171.1C276.7 504.6 267.2 512 255.9 512s-20.84-7.406-23.62-18.44l-42.66-171.2L18.47 279.6C7.406 276.8 0 267.3 0 255.1c0-11.34 7.406-20.83 18.44-23.61l171.2-42.78l42.78-171.1C235.2 7.406 244.7 0 256 0s20.84 7.406 23.62 18.44l42.78 171.2l171.2 42.78C504.6 235.2 512 244.6 512 255.1z'></svg></span>")
     }
--- script.js
+++ script.js
@@ -6,21 +6,11 @@ function toBottom() {
   $("html, body").scrollTop($(document).height())
 }
 
-selected_theme = 'black'
+var selected_theme = 'black'
 function setTheme(theme) {
   $("body").toggleClass(selected_theme)
   $("body").toggleClass(theme)
   selected_theme = theme
-}
-
-// update and post comments
-id = $(".comment:last-child").length ? $(".comment:last-child")[$(".comment:last-child").length - 1].id : 0
-xhttp = new XMLHttpRequest()
-interval = setTimeout(update, 5000) 
-xhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    addAnswer(this.responseText);
-  }
 }
 
 // send comment
@@ -28,7 +18,7 @@ $('.send').click(function () {
   xhttp.open("POST", window.location.href + "send.php")
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
 
-  try { id = $(".comment:last-child")[$(".comment:last-child").length - 1].id } catch { id = 0 }
+  let id = $(".comment:last-child").length ? $(".comment:last-child")[$(".comment:last-child").length - 1].id : 0
   xhttp.send('str=' + encodeURIComponent($(".input").val()) + '&id=' + id)
   $(".input").val('')
 })
@@ -38,15 +28,11 @@ function update () {
   xhttp.open("POST", window.location.href + "send.php")
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
 
-  try { id = $(".comment:last-child")[$(".comment:last-child").length - 1].id } catch { id = 0 }
+  let id = $(".comment:last-child").length ? $(".comment:last-child")[$(".comment:last-child").length - 1].id : 0
   xhttp.send("&id=" + id)
 }
 
-function addAnswer (data) {
-  clearInterval(interval)
-  interval = setInterval(update, 5000)
-  
-  console.log(data)
+function addAnswer(data) {
   data = JSON.parse(data)
   
   for (let i=0; i<data.length; i++) {
@@ -54,3 +40,15 @@ function addAnswer (data) {
     $('.comments').append(p)
   }
 }
+
+// update and post comments
+var xhttp = new XMLHttpRequest()
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    addAnswer(this.responseText);
+  }
+}
+
+$(document).ready(function() {
+  setInterval(update, 10000)
+})
